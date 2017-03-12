d1212391c7ef4562be295f3d7b1626176762d9e8
Francesco Giambruno <frank96990@gmail.com>
2017-03-12 17:02:33 +0100

    Update threeDimensionsArray.cpp

--- threeDimensionsArray.cpp
+++ threeDimensionsArray.cpp
@@ -6,13 +6,13 @@ main(){
 	for(int i = 0; i < 3; i++)
 		for(int j = 0; j < 4; j++)
 			for(int k = 0; k < 5; k++)
-				cin >> A[i][j][k];
+				cin >> A[i][j][k]; // read the array
 
 	cout << "start" << endl;
-	char c;
-	int f;
+	char c; // R for the rows, everything else for the colums 
+	int f; // number of the row [0..4] or column [0..5] for every level [0..2]
 	cin >> c >> f;
-	if(c == 'H'){
+	if(c == 'R'){
 		for(int i = 0; i < 3; i++)
 			for(int k = 0; k < 5; k++)
 				cout << A[i][f][k] << endl;
